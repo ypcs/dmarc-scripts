94a66d680621e42fbfec8fd3fee96334681a59fb
Ville Korhonen <ville@xd.fi>
2017-10-30 20:29:43 +0200

    Import latest version from upstream
    Import current version from <http://www.taugh.com/rddmarc/>.
    
    File modification dates:
    
        dmarcfail.py    2017-10-20 10:24
        mkdmarc         2016-05-01 17:47
        mkdmarc-update  2016-05-01 17:44
        mysql_ip6.c     2011-05-15 11:48
        rddmarc         2016-05-01 17:44
        readme          2016-05-01 17:46

--- rddmarc/dmarcfail.py
+++ rddmarc/dmarcfail.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python
-# $Header: /home/johnl/hack/dmarc/RCS/dmarcfail.py,v 1.1 2012/07/12 03:59:29 johnl Exp $
+# $Header: /home/johnl/hack/dmarc/RCS/dmarcfail.py,v 1.2 2015/05/02 20:15:02 johnl Exp $
 # parse DMARC failure reports, add it to the mysql database
 # optional arguments are names of files containing ARF messages,
 # otherwise it reads stdin
@@ -30,12 +30,15 @@
 # WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+#!/usr/local/bin/python
+# parse a DMARC failure report, add it to the mysql database
+
 import re
 import email
 import time
 import MySQLdb
 
-db = MySQLdb.connect(user='dmarc',passwd='xxx',db='dmarc', use_unicode=True)
+db = MySQLdb.connect(user='dmarc',passwd='x',db='dmarc', use_unicode=True)
 MySQLdb.paramstyle='format'
 
 def dmfail(h,f):
@@ -49,31 +52,44 @@ def dmfail(h,f):
             r = email.parser.Parser()
             fr = r.parsestr(p.get_payload()[0].as_string(), True)
             fx = re.search(r'<(.+?)@(.+?)>', fr['original-mail-from'])
-            origbox,origdom = fx.group(1,2)
-            arr = int(email.utils.mktime_tz(email.utils.parsedate_tz(fr['arrival-date'])))
+            if fx: origbox,origdom = fx.group(1,2)
+            else: origbox,origdom = (None,None)
+            if 'arrival-date' in fr:
+                arr = int(email.utils.mktime_tz(email.utils.parsedate_tz(fr['arrival-date'])))
+            else:                       # fake it with message date
+                arr = int(email.utils.mktime_tz(email.utils.parsedate_tz(e['date'])))
+            authres = fr['authentication-results'] if 'authentication-results' in fr else None
             
         elif(p.get_content_type() == "message/rfc822" or
             p.get_content_type() == "text/rfc822-headers"):
-            
-            m = email.message_from_string(p.get_payload())
+            if p.is_multipart():        # library thinks message/rfc822 is multipart
+                m = email.message_from_string(p.get_payload(0).as_string())
+            else:    
+                m = email.message_from_string(p.get_payload())
             frombox = fromdom = None
-            fx = re.search(r'<(.+?)@(.+?)>', m['from'])
-            if(fx): frombox,fromdom = fx.group(1,2)
-            else:
-                t = re.sub(m['from'],r"\s+|\([^)]*\)","")
-                fx = re.match(r'(.+?)@(.+?)', t)
-                if(fx): frombox,fromdom = fx.group(1,2)
+            if 'from' in m:
+                fx = re.search(r'<(.+)@(.+)>', m['from'])
+                if(fx):
+                    frombox,fromdom = fx.group(1,2)
+                else:
+                    t = re.sub(m['from'],r'\s+|\([^)]*\)',"")
+                    fx = re.match(r'(.+)@(.+)', t)
+                    if(fx):
+                        frombox,fromdom = fx.group(1,2)
 
     # OK, parsed it, now add an entry to the database
+    #print fr['reported-domain'],origdom,origbox,fromdom,frombox,arr,fr['source-ip'],"==="
+    #print m.as_string()
+    #print "==="
     c = db.cursor()
     c.execute("""INSERT INTO failure(serial,org,bouncedomain,bouncebox,fromdomain,
-        frombox,arrival,sourceip,headers)
-        VALUES(NULL,%s,%s,%s,%s,%s,FROM_UNIXTIME(%s),INET_ATON(%s),%s)""",
-        (fr['reported-domain'],origdom,origbox,fromdom,frombox,arr,fr['source-ip'],m.as_string()))
+        frombox,arrival,sourceip,headers,authres)
+        VALUES(NULL,%s,%s,%s,%s,%s,FROM_UNIXTIME(%s),INET_ATON(%s),%s,%s)""",
+        (fr['reported-domain'],origdom,origbox,fromdom,frombox,arr,fr['source-ip'],m.as_string(),authres))
+    c.execute("COMMIT")                 # in case it's a database with transactions
     print "Inserted failure report %s" % c.lastrowid
     c.close()
-
-#################################################################################################
+    
 if __name__ == "__main__":
     import sys
     
